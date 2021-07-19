5f30e2748ce94b5c1378ad1428fd7ea0b95a8a9e
Filipe Machado <filipe__machado@hotmail.com>
2021-07-19 01:23:54 -0300

    add rest_framework and test with basic game model

--- twitch_analyzer/src/models.py
+++ twitch_analyzer/src/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields import AutoField
+from django.utils import timezone
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=120)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = AutoDateTimeField(default=timezone.now)
+
--- twitch_analyzer/src/views.py
+++ twitch_analyzer/src/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, serializers
 
-# Create your views here.
+from src.models import Game
+
+class GameSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=120)
+
+
+class GameView(viewsets.ModelViewSet):
+    serializer_class = GameSerializer
+    queryset = Game.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
